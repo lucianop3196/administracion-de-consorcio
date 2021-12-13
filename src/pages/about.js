dbcca343f38f94f72ba7b630b3b3ba25319094bb
Luciano Pennacchioni <luciano.p3196@gmail.com>
2021-12-12 23:59:38 -0300

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import Layout from "../components/layout";
+// markup
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="Sobre Nosotros">
+      <p>
+        Esta sección es para contar sobre nosotros y el servicio que brindamos.
+      </p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
