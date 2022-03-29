340926ffb7069628c4ad1e466ccbec92a1023124
Luciano Pennacchioni <luciano.p3196@gmail.com>
2022-03-29 16:04:06 -0300

    StaticImage example

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 // markup
 const IndexPage = () => {
   return (
     <Layout pageTitle="Inicio">
       <p>Sitio web para Julian Tujague</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://images.unsplash.com/photo-1429667947446-3c93a979b7e0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+      />
     </Layout>
   );
 };
