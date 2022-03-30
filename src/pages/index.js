db7fc4fd54078d9ef897d5095bef3c3296883693
Luciano Pennacchioni <luciano.p3196@gmail.com>
2022-03-29 21:09:25 -0300

    re-order of folders

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import Layout from "../components/layout";
+import Layout from "../components/Layout/layout";
 import { StaticImage } from "gatsby-plugin-image";
 
 // markup
